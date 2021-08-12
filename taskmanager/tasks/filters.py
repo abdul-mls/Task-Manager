e06ff61f23647ad7ff97222339ca7aded55e5ce4
Muhammad <abdullah.m1230@gmail.com>
2021-08-12 17:41:37 +0100

    fourth commit

--- taskmanager/tasks/filters.py
+++ taskmanager/tasks/filters.py
@@ -5,4 +5,4 @@ from .models import *
 class OrderFilter(django_filters.FilterSet):
     class Meta:
         model = Task
-        fields = ('importance', )
+        fields = ('complete','importance', )
