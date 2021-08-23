24883e13942d0e302af94742d46d005b4bc226b2
Muhammad <abdullah.m1230@gmail.com>
2021-08-23 20:53:07 +0100

    update

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -15,9 +15,14 @@ urlpatterns = [
     path('task/<int:pk>/update/', TaskUpdate.as_view(), name="task_update"),
     path('task/<int:pk>/delete/', TaskDelete.as_view(), name="task_delete"),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(),name='reset_password'),
+    # path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    # path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="tasks/password_reset.html"), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="tasks/password_reset_sent.html"), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="tasks/password_reset_form.html"), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="tasks/password_reset_done.html"), name ='password_reset_complete'),
 
 ]
